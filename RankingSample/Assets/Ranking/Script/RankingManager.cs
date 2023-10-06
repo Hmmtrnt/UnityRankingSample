1a68df6bbbe5d59d31a3538cb2b8ff358e33ae30
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2023-10-06 14:24:16 +0900

    情報を受け取ることは成功

--- RankingSample/Assets/Ranking/Script/RankingManager.cs
+++ RankingSample/Assets/Ranking/Script/RankingManager.cs
@@ -19,6 +19,9 @@ public class RankingManager : MonoBehaviour
     const string _resetPass = @"ResetDB.php";
 
     /*uGUI*/
+    // hostURL
+    InputField _hostUrlField;
+
     // テーブルの作成
     InputField _createTableField;
     InputField _colum1TableField;
@@ -26,12 +29,20 @@ public class RankingManager : MonoBehaviour
     InputField _colum3TableField;
     // テーブルの受信
     Text _rankingLabel;
-    InputField _getTableFiled;
+    InputField _getTableField;
+    // テーブルの更新
+    InputField _updateTableField;
+    InputField _nameField;
+    InputField _scoreField;
+    // データベースの初期化
+
 
     // Start is called before the first frame update
     void Start()
     {
         /*uGUI*/
+        //hostURL
+        _hostUrlField = GameObject.Find("ServerUrlField").GetComponent<InputField>();
         // テーブル作成
         _createTableField = GameObject.Find("CreateTableField").GetComponent<InputField>();
         _colum1TableField = GameObject.Find("CreateColum1Field").GetComponent<InputField>();
@@ -39,7 +50,20 @@ public class RankingManager : MonoBehaviour
         _colum3TableField = GameObject.Find("CreateColum3Field").GetComponent<InputField>();
         // テーブルの受信
         _rankingLabel = GameObject.Find("TextRanking").GetComponent<Text>();
-        _getTableFiled = GameObject.Find("GetRankingTableField").GetComponent<InputField>();
+        _getTableField = GameObject.Find("GetRankingTableField").GetComponent<InputField>();
+
+        // テーブルの更新
+        _updateTableField = GameObject.Find("TextRanking").GetComponent<InputField>();
+        _nameField = GameObject.Find("NameField").GetComponent<InputField>();
+        _scoreField = GameObject.Find("ScoreField").GetComponent<InputField>();
+
+        /*input初期化*/
+        _createTableField.text = @"Ranking";
+        _colum1TableField.text = @"rank";
+        _colum2TableField.text = @"name";
+        _colum3TableField.text = @"score";
+        _getTableField.text = @"Ranking";
+        _hostUrlField.text = @"http://localhost/UnityRankingSample/CreateTable.php";
 
     }
 
@@ -55,10 +79,21 @@ public class RankingManager : MonoBehaviour
         StartCoroutine(GetRequest(_hostPass, _getPass));
     }
 
+    public void UpdateRanking()// ランキングの更新
+    {
+        StartCoroutine(PostRequest(_hostUrlField.text, _postPass));
+    }
+
     public void CreateTable()// テーブルの作成
     {
         // 非同期通信がいる、そのため別スレッドにメソッドを実行させる
         StartCoroutine(PostCreate(_hostPass, _createPass));
+    }
+
+    public void ResetDB()// データベースの初期化
+    {
+        // 
+        StartCoroutine(PostRequest(_hostUrlField.text, _resetPass));
     }
 
     /*Httpメソッド*/
@@ -69,7 +104,7 @@ public class RankingManager : MonoBehaviour
         string _getUrl = hostUrl + filePass;
 
         /*クエストリングの付与*/
-        _getUrl += @"?" + @"table=" + _getTableFiled.text;
+        _getUrl += @"?" + @"table=" + _getTableField.text;
 
         /*GetRequestの作成*/
         using UnityWebRequest _getRequest = UnityWebRequest.Get(_getUrl);
@@ -87,6 +122,38 @@ public class RankingManager : MonoBehaviour
             // 通信成功
             _rankingLabel.text = _getRequest.downloadHandler.text;
             Debug.Log(_getUrl);
+        }
+    }
+
+    IEnumerator PostRequest(string hostUrl, string postPass)
+    {
+        /*URLの作成*/
+        string _postUrl = hostUrl + postPass;
+
+        /*フォームの作成*/
+        WWWForm form = new WWWForm();
+        form.AddField("table", _getTableField.text);
+        form.AddField("name", _nameField.text);
+        form.AddField("score", _scoreField.text);
+
+        /*PostRequest*/
+        using UnityWebRequest _postRequest = UnityWebRequest.Post(_postUrl, form);
+
+        /*yield return*/
+        yield return _postRequest.SendWebRequest();
+
+        /*エラー処理*/
+        if (_postRequest.isNetworkError)
+        {
+            // 通信失敗
+            Debug.Log(_postRequest.error);
+            Debug.Log(_postUrl);
+        }
+        else
+        {
+            // 通信成功
+            Debug.Log(_postRequest.downloadHandler.text);
+            Debug.Log (_postUrl);
         }
     }
 
